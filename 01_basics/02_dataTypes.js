428cc87b09444ddd377d68802a03b97d91dbe1f2
Tanushree Soni <tssoni1206@gmail.com>
2024-12-27 03:20:54 +0530

    conversions

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(
+    3+
+    +3
+) //poor code readability
+
+let name = "tanu"
+let age = 21
+let idLoggedIn = false
+
+// number => 2^53
+//bigInt
+//string -> ""
+//boolean -> true/false
+//null => standalone value
+//undefined => value not assigned yet
+//symbol => unique
+
+//object
+console.log(typeof "Tanu")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "33"
+
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score1= "3a"
+// let valueInNumber1 = Number(score1) // a special type NaN
+// console.log(valueInNumber1);
+// console.log(typeof valueInNumber1);
+
+// let score2= null
+// let valueInNumber2 = Number(score1) // a special type NaN
+// console.log(valueInNumber2);
+// console.log(typeof valueInNumber2);
+
+// let score3= undefined
+// let valueInNumber3 = Number(score1) // a special type NaN
+// console.log(valueInNumber3);
+// console.log(typeof valueInNumber3);
+
+// score = true
+// console.log(Number(score));
+
+/* "33" => 33
+    "33abc" => NaN //Not a Number
+    true => 1; false => 0
+*/
+
+let isLoggedIn = 1
+isLoggedIn = "Tanu"
+isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+/*  1 => true; 0=>false
+    "" => false
+    "tanu" => true
+*/
+
+let num = 33
+console.log(String(num));
